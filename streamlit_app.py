42cd1eea0d00f774e0aade5d1da37ad3eae4d2d4
mani <mani@msei.ch>
2024-09-07 10:26:17 +0200

    Updated Get Help and About

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,8 @@ def set_page_config():
         layout="wide",
         initial_sidebar_state="expanded",
         menu_items={
-            'Get help': 'https://github.com/msei99/pbgui/#readme',
-            'About': "Passivbot GUI v1.12"
+            'Get help': 'https://github.com/msei99/pbgui-share/#readme',
+            'About': "PBGui Dashboard Share v0.95"
         }
     )
 
