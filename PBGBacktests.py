3cb6581a006ec258cc520ab2a35a4b626b4b0431
mani <mani@msei.ch>
2025-02-27 19:47:33 +0100

    Add Backtests

---
+++ PBGBacktests.py
@@ -0,0 +1,216 @@
+from pathlib import Path
+from time import sleep
+import sys
+from io import TextIOWrapper
+import traceback
+from User import Users
+import configparser
+from pathlib import Path, PurePath
+import subprocess
+import shutil
+import json
+from datetime import datetime, timedelta
+import glob
+import pandas as pd
+import plotly.graph_objects as go
+
+class PBGBacktests():
+    def __init__(self):
+        self.backtestsdir = None
+        self.pbgdir = None
+        self.pb7dir = None
+        self.pb7venv = None
+        self.pbgdir = None
+        self.ed = None
+        self.be = None
+        self.fills = None
+        self.load_ini()
+        self.users = Users()
+
+    def update_backtests(self):
+        update = False
+        for user in self.users:
+            if user.backtests:
+                for name, url in user.backtests.items():
+                    print(f'{datetime.now().isoformat(sep=" ", timespec="seconds")} Backtest {user.name} {name} {url}')
+                    self.backtest(user, name)
+                    update = True
+        if update:
+            print(f'{datetime.now().isoformat(sep=" ", timespec="seconds")} Update git')
+            self.update_git()
+    
+    def update_git(self):
+        try:
+            cmd = ['git', '-C', str(Path(self.backtestsdir).parent), 'add', 'api-service/*']
+            result = subprocess.run(cmd, capture_output=True, cwd=self.pbgdir, text=True, start_new_session=True)
+            if result.returncode != 0:
+                print(f'Error in git add: {result.stderr}')
+                print(f'{result.stdout}')
+                return
+
+            formatted_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            cmd = ['git', '-C', str(Path(self.backtestsdir).parent), 'commit', '-m', f'update backtests {formatted_time}']
+            result = subprocess.run(cmd, capture_output=True, cwd=self.pbgdir, text=True, start_new_session=True)
+            if result.returncode != 0:
+                print(f'Error in git commit: {result.stderr}')
+                print(f'{result.stdout}')
+                return
+
+            cmd = ['git', '-C', str(Path(self.backtestsdir).parent), 'push']
+            result = subprocess.run(cmd, capture_output=True, cwd=self.pbgdir, text=True, start_new_session=True)
+            if result.returncode != 0:
+                print(f'Error in git push: {result.stderr}')
+                print(f'{result.stdout}')
+                return
+
+        except Exception as e:
+            print(f'Exception occurred during git operations: {e}')
+            traceback.print_exc()
+    
+    def backtest(self, user, name):
+        # remove old backtest
+        backtest = Path.cwd() / 'backtests' / f'{user.name}_{name}'
+        if backtest.exists():
+            shutil.rmtree(backtest)
+        config = self.create_backtest_json(user, name)
+        cmd = [self.pb7venv, '-u', PurePath(f'{self.pb7dir}/src/backtest.py'), str(PurePath(f'{config}'))]
+        result = subprocess.run(cmd, capture_output=True, cwd=self.pb7dir, text=True, start_new_session=True)
+        if result.returncode == 0:
+            glob_be = f'{backtest}/**/balance_and_equity.csv'
+            be = glob.glob(glob_be, recursive=True)
+            if be:
+                self.load_be(be[0])
+                if self.be is not None:
+                    self.save_chart_be(f'{user.name}_{name}')
+            glob_fills = f'{backtest}/**/fills.csv'
+            fills = glob.glob(glob_fills, recursive=True)
+            if fills:
+                self.load_fills(fills[0])
+                if self.fills is not None:
+                    self.save_chart_symbol(f'{user.name}_{name}')
+
+    # Create Chart with plotly
+    def save_chart_be(self, file_name):
+        if self.be is not None:
+            fig = go.Figure()
+            fig.update_layout(yaxis_title='Balance')
+            fig.add_trace(go.Scatter(x=self.be['time'], y=self.be['equity'], name="equity", line=dict(width=0.75)))
+            fig.add_trace(go.Scatter(x=self.be['time'], y=self.be['balance'], name="balance", line=dict(width=2.5)))
+            fig.update_layout(yaxis_title='Balance', height=800)
+            fig.update_xaxes(showgrid=True, griddash="dot")
+            formatted_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            fig.update_layout(title_text=f'{file_name} {formatted_time}', title_x=0.5)
+            # save chart
+            fig.update_layout(template='plotly_dark', width=1920, height=1080)
+            fig.write_image(f'{self.backtestsdir}/{file_name}.png')
+
+    # Create Symbol Chart with plotly
+    def save_chart_symbol(self, file_name):
+        if self.fills is not None:
+            fig = go.Figure()
+            if "symbol" in self.fills:
+                coin_or_symbol = "symbol"
+            elif "coin" in self.fills:
+                coin_or_symbol = "coin"
+            for symbol in self.fills[coin_or_symbol].unique():
+                symbol_df = self.fills[self.fills[coin_or_symbol] == symbol].copy()
+                symbol_df["sym_pnl"] = symbol_df["pnl"].cumsum()
+                fig.add_trace(go.Scatter(x=symbol_df['time'], y=symbol_df['sym_pnl'], name=symbol))
+            # fig.update_layout(yaxis_title='PnL', height=800, )
+            fig['data'][0]['showlegend'] = True
+            fig.update_xaxes(showgrid=True, griddash="dot")
+            # save chart
+            fig.update_layout(template='plotly_dark', width=1920, height=1080)
+            fig.write_image(f'{self.backtestsdir}/{file_name}_symbol.png')
+
+    def load_be(self, be):
+        if Path(be).exists():
+            self.be = pd.read_csv(be)
+            timestamp = datetime.strptime(self.ed, '%Y-%m-%d').timestamp()
+            start_time = timestamp - (self.be.iloc[:, 0].iloc[-1] * 60)
+            self.be['time'] = datetime.fromtimestamp(start_time) + pd.to_timedelta(self.be.iloc[:, 0], unit='m')
+
+    def load_fills(self, fills):
+        if Path(fills).exists():
+            self.fills = pd.read_csv(fills)
+            timestamp = datetime.strptime(self.ed, '%Y-%m-%d').timestamp()
+            start_time = timestamp - (self.fills['minute'].iloc[-1] * 60)
+            self.fills['time'] = datetime.fromtimestamp(start_time) + pd.to_timedelta(self.fills['minute'], unit='m')
+
+    def create_backtest_json(self, user, name):
+        # copy config from user to backtests
+        config_src = Path(f'{self.pbgdir}/data/run_v7/{user.name}/config.json')
+        #config_dst = cwd + backtest
+        config_dst_dir = Path.cwd() / 'backtests'
+        if not config_dst_dir.exists():
+            config_dst_dir.mkdir()
+        config_dst = config_dst_dir / f'{user.name}_{name}.json'
+        shutil.copyfile(config_src, config_dst)
+        # update config with backtest url
+        with open(config_dst, 'r') as f:
+            config = json.load(f)
+        config['backtest']["base_dir"] = str(config_dst_dir / f'{user.name}_{name}')
+        if name == 'all':
+            config['backtest']["start_date"] = "2020-01-01"
+            # end_date = today - 1 day
+            config['backtest']["end_date"] = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
+        elif name == '2024':
+            config['backtest']["start_date"] = "2024-01-01"
+            config['backtest']["end_date"] = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
+        self.ed = config['backtest']["end_date"]
+        config['backtest']["exchanges"] = [user.exchange]
+        config['backtest']["combine_ohlcvs"] = False
+        # save config
+        with open(config_dst, 'w') as f:
+            json.dump(config, f, indent=4)
+        return config_dst
+
+    def load_ini(self):
+        pb_config = configparser.ConfigParser()
+        pb_config.read('pbgui-share.ini')
+        if pb_config.has_section("main"):
+            if pb_config.has_option("main", "pbgdir"):
+                self.pbgdir = pb_config.get("main", "pbgdir")
+            if pb_config.has_option("main", "backtestsdir"):
+                self.backtestsdir = pb_config.get("main", "backtestsdir")
+            if pb_config.has_option("main", "pb7dir"):
+                self.pb7dir = pb_config.get("main", "pb7dir")
+            if pb_config.has_option("main", "pb7venv"):
+                self.pb7venv = pb_config.get("main", "pb7venv")
+    
+def main():
+    pbbacktests = PBGBacktests()
+    if not pbbacktests.pbgdir:
+        print(f'pbgdir is not defined in pbgui-share.ini')
+        return
+    if not pbbacktests.backtestsdir:
+        print(f'backtestsdir is not defined in pbgui-share.ini')
+        return
+    if not pbbacktests.pb7dir:
+        print(f'pb7dir is not defined in pbgui-share.ini')
+        return
+    if not pbbacktests.pb7venv:
+        print(f'pb7venv is not defined in pbgui-share.ini')
+        return
+    # Init logfile
+    logfile = Path(f'PBGBacktests.log')
+    sys.stdout = TextIOWrapper(open(logfile,"ab",0), write_through=True)
+    sys.stderr = TextIOWrapper(open(logfile,"ab",0), write_through=True)
+    print(f'{datetime.now().isoformat(sep=" ", timespec="seconds")} Start: PBGBacktests')
+    while True:
+        try:
+            if logfile.exists():
+                if logfile.stat().st_size >= 10485760:
+                    logfile.replace(f'{str(logfile)}.old')
+                    sys.stdout = TextIOWrapper(open(logfile,"ab",0), write_through=True)
+                    sys.stderr = TextIOWrapper(open(logfile,"ab",0), write_through=True)
+            pbbacktests.update_backtests()
+            print(f'{datetime.now().isoformat(sep=" ", timespec="seconds")} Sleep for 5 minutes')
+            sleep(300)
+            pbbacktests.users.load()
+        except Exception as e:
+            print(f'Something went wrong, but continue {e}')
+            traceback.print_exc()
+
+if __name__ == '__main__':
+    main()
