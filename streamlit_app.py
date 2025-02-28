a73689b29a88c6af3c25f42cc60036d62917ae43
mani <mani@msei.ch>
2025-02-28 19:13:56 +0100

    symbol

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,11 +37,11 @@ def view():
             symbols = ""
             for year in range(2020, datetime.now().year + 1):
                 url = user.backtests + f'/{user.name}_{year}.png?raw=true'
-                url_symbols = user.backtests + f'/{user.name}_{year}_symbols.png?raw=true'
+                url_symbols = user.backtests + f'/{user.name}_{year}_symbol.png?raw=true'
                 symbols = symbols + f"[{year}](%s) " % url_symbols
                 backtests = backtests + f"[{year}](%s) " % url
             url = user.backtests + f'/{user.name}_all.png?raw=true'
-            url_symbols = user.backtests + f'/{user.name}_all_symbols.png?raw=true'
+            url_symbols = user.backtests + f'/{user.name}_all_symbol.png?raw=true'
             symbols = symbols + f"[all](%s) " % url_symbols
             backtests = backtests + f"[all](%s) " % url
             st.markdown('##### Backtest: ' + backtests)
